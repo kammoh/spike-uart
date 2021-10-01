f3a954e3c5b06a2344f12528184e8c1e4d6f7d98
Kamyar Mohajerani <kammoh@gmail.com>
2021-10-01 05:25:13 -0400

    .

--- plugin_test/src/main.rs
+++ plugin_test/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
+#![feature(global_asm)]
 
 extern crate panic_halt;
 extern crate riscv_rt;
@@ -8,6 +10,29 @@ use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    // do something here
+    unsafe {
+        // asm!("li t0, 0x10000000");
+        // asm!("lw t1, 0(t0)");
+        // asm!("sw t1, 0(t0)");
+        // Write the value 1 to tohost, telling Spike to quit with an exit code of 0.
+        // asm!("li t0, 1");
+        // asm!("la t1, tohost");
+        // asm!("sw t0, 0(t1)");
+
+        // Spin until Spike terminates the run.
+        // asm!("j 1b");
+    }
     loop {}
 }
+
+
+// global_asm!(
+//     ".align 3",
+//     ".global tohost",
+//     "tohost:",
+//     ".dword 0",
+//     ".align 3",
+//     ".global fromhost",
+//     "fromhost:",
+//     ".dword 0"
+// );
